5c0a32fc31ed7b029f152c27c58afee08e9b467c
taufiq-aminudin <internationalsuryautama@gmail.com>
2026-08-26 06:47:32 +0700

    Update index.js

--- cloudflare/index.js
+++ cloudflare/index.js
@@ -1,1 +1,15 @@
+// cloudflare/index.js
+import handleAdminLogin from "./routes/superAdminLogin.js";
+
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/api/admin/login" && request.method === "POST") {
+      return handleAdminLogin(request);
+    }
+
+    return new Response("Not Found", { status: 404 });
+  }
+};
 
